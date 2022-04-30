3ab788d591bfb939308c71016150a538e937e7df
unknown <nguyenpeter.edu@gmail.com>
2022-04-30 20:21:13 +0700

    add more songs

--- src/seeds/songsData.js
+++ src/seeds/songsData.js
@@ -86,5 +86,117 @@ export const songsData = [
         img: 'images/and_one.jpg',
         singer: 'Tae Yeon (SNSD)',
         album: 'The Wind Blows OST'
-    }
+    },
+    {
+        id: 12,
+        name: 'Phố Mùa Đông',
+        path: 'music/pho_mua_dong.mp3',
+        img: 'images/pho_mua_dong.jpg',
+        singer: 'Hà Anh Tuấn',
+        album: 'SEE SING SHARE 2'
+    },
+    {
+        id: 13,
+        name: 'Cứ Thế',
+        path: 'music/cu_the.mp3',
+        img: 'images/cu_the.jpg',
+        singer: 'Hà Anh Tuấn',
+        album: 'Tuổi Thanh Xuân OST'
+    },
+    {
+        id: 14,
+        name: 'TÔI LÀ AI TRONG EM',
+        path: 'music/toi_la_ai_trong_em.mp3',
+        img: 'images/toi_la_ai_trong_em.jpg',
+        singer: 'ERIK',
+        album: 'Taxi! Em Tên Gì OST'
+    },
+    {
+        id: 15,
+        name: 'Mưa Phi Trường',
+        path: 'music/mua_phi_truong.mp3',
+        img: 'images/mua_phi_truong.jpg',
+        singer: 'Hà Anh Tuấn',
+        album: 'SEE SING SHARE 3'
+    },
+    {
+        id: 16,
+        name: 'For You',
+        path: 'music/for_you.mp3',
+        img: 'images/for_you.jpg',
+        singer: 'EXO',
+        album: 'Moonlovers: Scarlet Heart Ryeo OST'
+    },
+    {
+        id: 17,
+        name: 'XUÂN THÌ',
+        path: 'music/xuan_thi.mp3',
+        img: 'images/xuan_thi.jpg',
+        singer: 'HÀ ANH TUẤN',
+        album: 'Tuyết rơi mùa hè'
+    },
+    {
+        id: 18,
+        name: 'Tháng Mấy Em Nhớ Anh?',
+        path: 'music/thang_may_em_nho_anh.mp3',
+        img: 'images/thang_may_em_nho_anh.jpg',
+        singer: 'HÀ ANH TUẤN',
+        album: 'Tháng Mấy Em Nhớ Anh?'
+    },
+    {
+        id: 19,
+        name: 'Tháng Tư Là Lời Nói Dối Của Em',
+        path: 'music/thang_tu_la_loi_noi_doi_cua_em.mp3',
+        img: 'images/thang_tu_la_loi_noi_doi_cua_em.jpg',
+        singer: 'HÀ ANH TUẤN',
+        album: 'Fragile'
+    },
+    {
+        id: 20,
+        name: 'TÌNH NÀO KHÔNG NHƯ TÌNH ĐẦU',
+        path: 'music/tinh_nao_khong_nhu_tinh_dau.mp3',
+        img: 'images/tinh_nao_khong_nhu_tinh_dau.jpg',
+        singer: 'TRUNG QUÂN',
+        album: 'TÌNH NÀO KHÔNG NHƯ TÌNH ĐẦU (Single)'
+    },
+    {
+        id: 21,
+        name: 'BƯỚC QUA NHAU',
+        path: 'music/buoc_qua_nhau.mp3',
+        img: 'images/buoc_qua_nhau.jpg',
+        singer: 'Vũ',
+        album: 'BƯỚC QUA NHAU (Single)'
+    },
+    {
+        id: 22,
+        name: 'BƯỚC QUA MÙA CÔ ĐƠN',
+        path: 'music/buoc_qua_mua_co_don.mp3',
+        img: 'images/buoc_qua_mua_co_don.jpg',
+        singer: 'Vũ',
+        album: 'BƯỚC QUA MÙA CÔ ĐƠN (Single)'
+    },
+    {
+        id: 23,
+        name: 'NGÀY ĐẦU TIÊN',
+        path: 'music/ngay_dau_tien.mp3',
+        img: 'images/ngay_dau_tien.jpg',
+        singer: 'ĐỨC PHÚC',
+        album: 'NGÀY ĐẦU TIÊN (Single)'
+    },
+    {
+        id: 24,
+        name: 'Cơn mưa tình yêu',
+        path: 'music/con_mua_tinh_yeu.mp3',
+        img: 'images/con_mua_tinh_yeu.jpg',
+        singer: 'Hà Anh Tuấn & Phương Linh',
+        album: 'Ngày hát đôi…'
+    },
+    {
+        id: 25,
+        name: 'I Do',
+        path: 'music/i_do.mp3',
+        img: 'images/i_do.jpg',
+        singer: '911',
+        album: 'I Do (Single)'
+    },
 ]
