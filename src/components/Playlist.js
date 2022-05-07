ad1c7566cf5378198d3e490d28d43444d5f1fd1e
unknown <nguyenpeter.edu@gmail.com>
2022-05-07 16:08:21 +0700

    remove unused code

--- src/components/Playlist.js
+++ src/components/Playlist.js
@@ -13,7 +13,6 @@ export default function Playlist() {
         songs,
         currentIndex,
         playThisSong,
-        openPlaylistModal,
         currentPlaylist,
         moveSongToTopList,
         allPlaylists,
